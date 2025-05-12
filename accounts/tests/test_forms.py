8b0f661043af71d85a95a4745168c01d81ac7466
Микитюк Роман <romamykytiuk0604@gmail.com>
2025-05-12 20:05:48 +0300

    комент2

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from accounts.forms import UserRegistrationForm
+from accounts.forms import UserRegistrationForm,LoginForm
 from accounts.models import User
 
 class UserRegistrationFormTests(TestCase):
@@ -12,6 +12,7 @@ class UserRegistrationFormTests(TestCase):
             'confirm_password':'0604',
         })
         self.assertTrue(form.is_valid())
+
     def test_invalid_when_email_empty(self):
         form = UserRegistrationForm(data={
             'username': 'testuser1',
@@ -22,7 +23,6 @@ class UserRegistrationFormTests(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('email',form.errors)
 
-
     def test_invalid_when_username_empty(self):
         form = UserRegistrationForm(data={
             'username': '',
@@ -32,6 +32,7 @@ class UserRegistrationFormTests(TestCase):
         })
         self.assertFalse(form.is_valid())
         self.assertIn('username', form.errors)
+
     def test_duplicate_username(self):
         User.objects.create_user(
             username='test42',
@@ -58,4 +59,51 @@ class UserRegistrationFormTests(TestCase):
         self.assertFalse(form.is_valid())
 
 
+    def test_duplicate_email(self):
+        User.objects.create_user(
+            username='test4',
+            email='test0@gmail.com',
+            password='0604',
+        )
+        form = UserRegistrationForm(data={
+            'username': 'test0',
+            'email': 'test0@gmail.com',
+            'password': '0604',
+            'confirm_password': '0604',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors)
+
+class UserLoginFormTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test',
+            email='test@gmail.com',
+            password='0604',
+        )
+
+    def test_login_with_username(self):
+        form = LoginForm(data={
+            'login': 'test',
+            'password': '0604',
+        })
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.user,self.user)
+
+    def test_login_with_email(self):
+        form = LoginForm(data={
+            'login': 'test@gmail.com',
+            'password': '0604',
+        })
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.user, self.user)
+
+    def test_invalid_username(self):
+        form = LoginForm(data={
+            'login': 'test1',
+            'password': '0604',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('__all__',form.errors)
+        self.assertEqual(form.errors['__all__'][0],'невірний username/email  або пароль')
 
